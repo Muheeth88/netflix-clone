4a8886c616caef298a67edcac582c419a32f30f8
Muheeth88 <abdulmuheeth88@gmail.com>
2022-09-27 18:39:56 +0530

    rows finished

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
       <h1>Netflix - Clone</h1>
       <Row title="NETFLIX ORIGINALS"  fetchUrl={requests.fetchNetflixOriginals}/>
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      {/* <Row title="Top Rated" fetchUrl={requests.fetchTopRated} /> */}
+      <Row title="Action" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/requests.js
+++ src/requests.js
@@ -2,11 +2,11 @@ const API_KEY = "1d7b1bc3675f9d63db16b9f126342703";
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRated: `/movies/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
     fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchHorrorMocies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
     fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
     fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
 }
